a6a55ad8339ca2ce16a28ae0c4c3319980f0ca04
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-02-06 04:41:26 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -6,27 +6,27 @@ import Review from './Components/Review/Review';
 import Inventory from './Components/Inventory/Inventory';
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <Header></Header>
-        <Switch>
-          <Route exact path='/'>
-            <Shop></Shop>
-          </Route>
-          <Route path='/shop'>
-            <Shop></Shop>
-          </Route>
-          <Route path='/review'>
-            <Review></Review>
-          </Route>
-          <Route path='/Inventory'>
-            <Inventory></Inventory>
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Header></Header>
+                <Switch>
+                    <Route exact path='/'>
+                        <Shop></Shop>
+                    </Route>
+                    <Route path='/shop'>
+                        <Shop></Shop>
+                    </Route>
+                    <Route path='/review'>
+                        <Review></Review>
+                    </Route>
+                    <Route path='/Inventory'>
+                        <Inventory></Inventory>
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import './Header';
-
+import './Header.css';
+import logo from './../../images/logo.png';
+import { NavLink } from 'react-router-dom';
 const Header = () => {
-  return (
-    <div>
-      <h1>This is Header</h1>
-    </div>
-  );
+    return (
+        <div>
+            <div className='text-center'>
+                <img src={logo} alt='' id='logo' />
+            </div>
+            <div className='bg-black' id='navbar'>
+                <NavLink to='/shop' className='nav-link-1'>
+                    Shop
+                </NavLink>
+                <NavLink to='/review' className='nav-link-1'>
+                    Order Review
+                </NavLink>
+                <NavLink to='/inventory' className='nav-link-1'>
+                    Inventory
+                </NavLink>
+            </div>
+        </div>
+    );
 };
 
 export default Header;

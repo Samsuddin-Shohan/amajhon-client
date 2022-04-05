53cd42999631be8b834ae0b1616519e331fd2481
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-04-05 16:55:13 +0600

    env file added

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: 'AIzaSyC9EMchwIxJKf2e8k_ttCstErfuktJ5jPg',
-    authDomain: 'amajhon-cc2f1.firebaseapp.com',
-    projectId: 'amajhon-cc2f1',
-    storageBucket: 'amajhon-cc2f1.appspot.com',
-    messagingSenderId: '909335009717',
-    appId: '1:909335009717:web:608c609268a2b59fe421fc',
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
 };
 export default firebaseConfig;
